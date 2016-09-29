69ba9e8875139f8c74a2d5e1ee6a08ae14e4629b
Derek Chiu <dchiu@bccrc.ca>
2016-09-28 17:38:01 -0700

    use updated function names, fix some docs

--- R/conClustEnsemble.R
+++ R/conClustEnsemble.R
@@ -1,20 +1,22 @@
-#' Title : function to generate a cluster ensemble from the data set X using K-means algorithm
+#' Generate a cluster ensemble using the K-means algorithm
 #'
-#' @param X : a data set, rows are observations, columns are variables
-#' @param M : number of base clusterings in the ensemble
-#' @param k : number of clusters in the base clusterings
-#' @param scheme : cluster ensemble generating scheme (1=fixed k, 2=random k)
+#' @param X a data set, rows are observations, columns are variables
+#' @param M number of base clusterings in the ensemble
+#' @param k number of clusters in the base clusterings
+#' @param scheme cluster ensemble generating scheme (1=fixed k, 2=random k)
 #'
-#' @return E: matrix of cluster ensemble
+#' @return matrix of cluster ensemble
+#' @author Johnson Liu
+#' @importFrom stats complete.cases
 #' @export
-conClustEnsemble <- function(X, k, reps = 1000) {
+ConClustEnsemble <- function(X, k, reps = 1000) {
   assertthat::assert_that(is.matrix(X))
   E <- NULL
   df_conClust <- data.frame(t(X))
   colnames(df_conClust) <- rownames(X)
   rownames(df_conClust) <- colnames(X)
   E <-
-    ConClust::ConClust(
+    ConClust(
       df_conClust,
       k = k,
       reps = reps,
@@ -39,15 +41,15 @@ conClustEnsemble <- function(X, k, reps = 1000) {
     E_imputed <- array(dim = dim(E))
   }
   for (i in 1:dim(E_imputed)[3]) {
-    E_imputed[, , i] <- apply(E[, , i], 2, knnImpute, tdat = X)
+    E_imputed[, , i] <- apply(E[, , i], 2, knn_impute, data = X)
   }
-  E_cs <- consensusSummaries(E_imputed, k = k)
+  E_cs <- consensus_summary(E_imputed, k = k)
   for(i in 2:length(E_cs)){
-    E_cs[[i]]$consensusClass<-classRelabel(E_cs[[1]]$consensusClass,E_cs[[i]]$consensusClass)
+    E_cs[[i]]$consensusClass<-relabel_class(E_cs[[1]]$consensusClass,E_cs[[i]]$consensusClass)
   }
   for (i in 1:length(E_cs)) {
     E_imputed[, , i] <-
-      apply(E_imputed[, , i], 2, classRelabel, cl.ref = E_cs[[i]]$consensusClass)
+      apply(E_imputed[, , i], 2, relabel_class, cl.ref = E_cs[[i]]$consensusClass)
   }
   E_imputed_relabelled <- E_imputed
   E_imputed_relabelled_voted <- array(dim = dim(E_imputed_relabelled))
