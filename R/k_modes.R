3d9cc4bc2c35e6a7bf04b390974dd997c10dcae4
Derek Chiu <dchiu@bccrc.ca>
2016-09-30 14:39:40 -0700

    shorten example to < 5s, fix formatting and unit test

--- R/k_modes.R
+++ R/k_modes.R
@@ -1,34 +1,43 @@
 #' Combine clustering results using K modes
-#'
-#' Combine clustering results generated using different algorithms and different data perturbations
-#' @param E a matrix of clusterings with number of rows equal to the number of cases to be clustered, number of columns equal to the clustering obtained by different resampling of the data, and the third dimension is the different algorithms. Needs complete cases: No NAs. If the matrix contains NAs those are imputed by majority voting (after class relabeling).
-#' @param is.relabelled TRUE/FALSE defaults to TRUE, but if FALSE the data will be relabelled
+#' 
+#' Combine clustering results generated using different algorithms and different
+#' data perturbations
+#' 
+#' @param E a matrix of clusterings with number of rows equal to the number of
+#'   cases to be clustered, number of columns equal to the clustering obtained
+#'   by different resampling of the data, and the third dimension is the
+#'   different algorithms. Needs complete cases: No NAs. If the matrix contains
+#'   NAs those are imputed by majority voting (after class relabeling).
+#' @param is.relabelled logical; defaults to TRUE, but if FALSE the data will
+#'   be relabelled
 #' @return a vector of cluster assignment based on kmodes
 #' @author Aline Talhouk
 #' @export
 #' @examples
+#' # Calculate for a fraction of first algorithm
 #' data(E_imputed)
-#' table(k_modes(E_imputed, is.relabelled=FALSE))
-
-k_modes <- function(E, is.relabelled=TRUE){
-#take E imputed and reshape into a flat matrix
-flat_E <- E
-dim(flat_E) <- c(dim(E)[1],dim(E)[2]*dim(E)[3])
-
-if(is.relabelled==FALSE){
-  E_f <- apply(flat_E[,-1],2,function(x){relabel_class(x,flat_E[,1])})
-  flat_E <- cbind(flat_E[,1],E_f)
+#' table(k_modes(E_imputed[1:100, , 1, drop = FALSE], is.relabelled = FALSE))
+k_modes <- function(E, is.relabelled = TRUE) {
+  # take E imputed and reshape into a flat matrix
+  flat_E <- E
+  dim(flat_E) <- c(dim(E)[1], dim(E)[2] * dim(E)[3])
+  if (is.relabelled == FALSE) {
+    E_f <- apply(flat_E[, -1], 2, function(x) {
+      relabel_class(x, flat_E[, 1])
+    })
+    flat_E <- cbind(flat_E[, 1], E_f)
   }
-
-
-# Fill in missing values if any using majority voting
-if(anyNA(flat_E)){
-  flat_E <- t(apply(flat_E,1,
-                  function(x){x[which(is.na(x))] <- names(which.max(table(x)));x}))
-}
-
-dim(flat_E)
-k_modes <- klaR::kmodes(flat_E,modes = max(apply(flat_E,2,
-                                function(x){as.numeric(names(table(x)))})))
-return(k_modes$cluster)
+  # Fill in missing values if any using majority voting
+  if (anyNA(flat_E)) {
+    flat_E <- t(apply(flat_E, 1, function(x) {
+      x[which(is.na(x))] <- names(which.max(table(x)))
+      return(x)
+    }))
+  }
+  k_modes <- klaR::kmodes(
+    flat_E,
+    modes = max(unlist(apply(flat_E, 2, function(x)
+      as.numeric(names(table(x))))))
+  )
+  return(k_modes$cluster)
 }
--- tests/testthat/test-consensus_functions.R
+++ tests/testthat/test-consensus_functions.R
@@ -1,17 +1,21 @@
-context("Consensus functions")
+
+context("Majority voting and K modes")
 
 test_that("majority voting works", {
-  dt = array(c(2,3,2,2,2,3,1,1,2,3,2,2,2,3,1,1),c(2,4,2))
-  expect_equal( majority_voting(dt, is.relabelled = TRUE), c("2","3"))
-  })
+  dt <- array(c(2, 3, 2, 2, 2, 3, 1, 1, 2, 3, 2, 2, 2, 3, 1, 1), c(2, 4, 2))
+  expect_equal(majority_voting(dt, is.relabelled = TRUE), c("2", "3"))
+})
 
-
-test_that("k modes", {
+test_that("k modes works", {
   set.seed(1)
   x <- rbind(matrix(rbinom(250, 2, 0.25), ncol = 5),
              matrix(rbinom(250, 2, 0.75), ncol = 5))
   colnames(x) <- c("a", "b", "c", "d", "e")
   xp <- x
-  dim(xp) <- c(10,5,10)
-  expect_equal(k_modes(xp ,is.relabelled = TRUE), kmodes(x,2))
+  dim(xp) <- c(10, 5, 10)
+  set.seed(1)
+  kmo.old <- klaR::kmodes(xp, 2)$cluster
+  set.seed(1)
+  kmo.new <- k_modes(xp, is.relabelled = TRUE)
+  expect_equal(kmo.old, kmo.new)
 })
