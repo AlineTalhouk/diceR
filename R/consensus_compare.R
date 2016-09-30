ee0e2b344d334343e5d65619966ac3e95cf3c5ae
Derek Chiu <dchiu@bccrc.ca>
2016-09-30 11:47:26 -0700

    update doc, remove rownames using mutate

--- R/consensus_compare.R
+++ R/consensus_compare.R
@@ -1,4 +1,4 @@
-#' @param data data matrix with samples as rows, genes as columns
+#' @param data data matrix with genes as rows, samples as columns
 #' @param cl.mat matrix of cluster assignments. Each row is an assignment for a
 #'   different algorithm. Use \code{element = "class"} in
 #'   \code{consensus_combine}.
@@ -15,8 +15,8 @@ consensus_compare <- function(data, cl.mat, cons.mat, alg.names = NULL) {
   } else {
     an <- names(cons.mat)
   }
-  ind <- data.frame(Algorithms = an,
-                    PAC = sapply(cons.mat, PAC, lower = 0.05, upper = 0.95),
-                    CHI = apply(cl.mat, 2, clusterSim::index.G1, x = t(data)))
+  ind <- data.frame(Algorithms = an) %>% 
+    mutate(PAC = sapply(cons.mat, PAC, lower = 0.05, upper = 0.95),
+           CHI = apply(cl.mat, 2, clusterSim::index.G1, x = t(data)))
   return(ind)
 }
