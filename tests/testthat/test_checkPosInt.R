47920d7a4dee6780fc333f2be7d3cd8c34e91c92
gliu@bccrc.ca <g.liu@alumni.ubc.ca>
2016-10-04 15:13:05 -0700

    added tests

---
+++ tests/testthat/test_checkPosInt.R
@@ -0,0 +1,12 @@
+test_that("checkPosInt",{
+  expect_true(checkPosInt(3))
+  expect_false(checkPosInt(-3))
+  expect_true(checkPosInt(1e6))
+  expect_false(checkPosInt(3.6))
+  expect_false(checkPosInt(3.21e1))
+  expect_false(checkPosInt(-3.7))
+  expect_false(checkPosInt(-3.21e1))
+  expect_error(checkPosInt(c(1,2,3)))
+  expect_error(checkPosInt("a"))
+  expect_error(checkPosInt(matrix(c(1,2,3),nrow=1)))
+})
