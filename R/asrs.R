90fd5236b10a692b74868f0712da9d86343d4960
gliu@bccrc.ca <g.liu@alumni.ubc.ca>
2016-10-04 11:59:34 -0700

    moved asrs,cts,relabelCl,srs

---
+++ R/asrs.R
@@ -0,0 +1,59 @@
+#' Title A function for computing approximated simrank based similarity matrix
+#'
+#' @param E : matrix of cluster ensemble
+#' @param dc :decay factor, range 0 to 1, inclusive
+#'
+#' @return S: ASRS matrix
+#' @export
+#'
+#' @examples
+asrs<-function(E,dc){
+  assertthat::assert_that(is.matrix(E))
+  assert_that(is.numeric(E))
+  assertthat::assert_that(dc>=0 && dc<=1)
+  n<-nrow(E)
+  M<-ncol(E)
+  E.new<-relabelCl(E)
+  E<-E.new$newE
+  no_allcl<-E.new$no_allcl
+  wcl<-weightCl(E)
+  CS<-matrix(rep(0,no_allcl*no_allcl),nrow=no_allcl)
+  for(i in 1:(no_allcl-1)){
+    Ni<-wcl[i,]
+    ni<-length(Ni[which(Ni>0)])
+    for(j in (i+1):no_allcl){
+      Nj<-wcl[j,]
+      nj<-length(Nj[which(Nj>0)])
+      if((ni*nj)>0){
+        CS[i,j]<-(Ni%*%Nj)/(ni*nj)
+      }
+    }
+  }
+  if(max(CS)>0){
+    CS<-CS/max(CS)
+  }
+  CS<-CS+t(CS)
+  for(i in 1:no_allcl){
+    CS[i,i]<-1
+  }
+  S<-matrix(rep(0,n*n),nrow=n)
+  for(i in 1:(n-1)){
+    for(ii in (i+1):n){
+      for(j in 1:M){
+        for(jj in 1:M){
+          if(CS[E[i,j],E[ii,jj]]==1){
+            S[i,ii]<-S[i,ii]+1
+          } else{
+            S[i,ii]<-S[i,ii]+dc*CS[E[i,j],E[ii,jj]]
+          }
+        }
+      }
+    }
+  }
+  S<-S/(M*M)
+  S<-S+t(S)
+  for(i in 1:n){
+    S[i,i]<-1
+  }
+  return(S)
+}
---
+++ R/srs.R
@@ -0,0 +1,71 @@
+#' Title function for computing simrank based similarity matrix
+#'
+#' @param E : matrix of cluster ensemble
+#' @param dc : decay factor, [0,1]
+#' @param R : number of iterations for simrank algorithm
+#'
+#' @return
+#' @export
+#'
+#' @examples
+srs<-function(E,dc,R){
+  assert_that(is.matrix(E))
+  assert_that(dc>=0 && dc<=1)
+  assert_that(is.numeric(R))
+  assert_that(checkPosInt(R)==TRUE)
+  n<-nrow(E)
+  M<-ncol(E)
+  E.new<-relabelCl(E)
+  E<-E.new$newE
+  no_allcl<-E.new$no_allcl
+  S<-diag(x=1,nrow=n,ncol=n)
+  C<-diag(x=1,nrow=no_allcl,ncol=no_allcl)
+
+  for(r in 1:(R-1)){
+    S1<-diag(x=1,nrow=n,ncol=n)
+    for(i in 1:(n-1)){
+      Ni<-E[i,]
+      for(ii in (i+1):n){
+        sum_sim<-0
+        Nii<-E[ii,]
+        for(k in 1:M){
+          for(kk in 1:M){
+            sum_sim<-sum_sim+C[Ni[k],Nii[kk]]
+          }
+        }
+        S1[i,ii]<-(dc/(M*M))*sum_sim
+      }
+    }
+    S1<-S1+t(S1)
+    for(i in 1:n){
+      S1[i,i]<-1
+    }
+    C1<-diag(x=1,nrow=no_allcl,ncol=no_allcl)
+    for(i in 1:(no_allcl-1)){
+      Ni<-getRowColNumbers(E,i)$rows
+      col<-getRowColNumbers(E,i)$cols
+      nki<-length(Ni)
+      for(ii in (i+1):no_allcl){
+        sum_sim<-0
+        Nii<-getRowColNumbers(E,ii)$rows
+        col<-getRowColNumbers(E,ii)$cols
+        nkii<-length(Nii)
+        for(k in 1:nki){
+          for(kk in 1:nkii){
+            sum_sim<-sum_sim+S[Ni[k],Nii[kk]]
+          }
+        }
+        if((nki*nkii)>0){
+          C1[i,ii]<-dc*sum_sim/(nki*nkii)
+        }
+      }
+    }
+    C1<-C1+t(C1)
+    for(i in 1:no_allcl){
+      C1[i,i]<-1
+    }
+    S<-S1
+    C<-C1
+  }
+  return(S)
+}
