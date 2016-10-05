d2ead89d51524aba618b5de4d5045156aa084c58
Derek Chiu <dchiu@bccrc.ca>
2016-10-05 11:03:18 -0700

    document E_LCE data

--- R/data.R
+++ R/data.R
@@ -5,6 +5,13 @@
 #' @format An array with 489 rows, 10 columns, 12 slices
 "E_imputed"
 
+#' Example data for Linkage Cluster Ensemble (LCE)
+#' 
+#' The data set is intended to be used with the LCE family of functions
+#' 
+#' @format A matrix with 100 rows (samples) and 10 columns (cluster assignments).
+"E_LCE"
+
 #' High grade serous cancer data from TCGA used to classify subtypes
 #'
 #' There are 321 genes and 489 samples. The first column is a gene identifier.
