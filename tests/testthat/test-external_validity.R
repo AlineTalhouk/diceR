154d66845eb3780968ae66d3f22ae9682fbfbb98
gliu@bccrc.ca <g.liu@alumni.ubc.ca>
2016-10-06 16:38:46 -0700

    added data objects for validility scores and in tests

--- tests/testthat/test-external_validity.R
+++ tests/testthat/test-external_validity.R
@@ -1,7 +1,7 @@
 
 context("External validity indices")
 
-data("FGT", "FGD", "LT", "LD", "E",
+data("FGT", "FGD", "LT", "LD", "E_LCE",
      "final_c1_valid_RandIndex", "final_c2_valid_RandIndex")
 
 test_that("Check ev_accuracy works with basic inputs", {
@@ -18,11 +18,11 @@ test_that("Check ev_accuracy throws error with wrong inputs", {
   expect_equal(ev_accuracy(c("a", "a", "b"), c("a", "b", "c")), 2 / 3)
 })
 
-test_that("Check ev_rand with E and in case where AR is non-zero", {
-  expect_true(abs(ev_rand(E[, 1], E[, 2])$AR - 0.2732) <= 0.0001)
-  expect_true(abs(ev_rand(E[, 1], E[, 2])$HI - 0.3446) <= 0.0001)
-  expect_true(abs(ev_rand(E[, 1], E[, 2])$MI - 0.3277) <= 0.0001)
-  expect_true(abs(ev_rand(E[, 1], E[, 2])$RI - 0.6723) <= 0.0001)
+test_that("Check ev_rand with E_LCE and in case where AR is non-zero", {
+  expect_true(abs(ev_rand(E_LCE[, 1], E_LCE[, 2])$AR - 0.2732) <= 0.0001)
+  expect_true(abs(ev_rand(E_LCE[, 1], E_LCE[, 2])$HI - 0.3446) <= 0.0001)
+  expect_true(abs(ev_rand(E_LCE[, 1], E_LCE[, 2])$MI - 0.3277) <= 0.0001)
+  expect_true(abs(ev_rand(E_LCE[, 1], E_LCE[, 2])$RI - 0.6723) <= 0.0001)
   expect_true(ev_rand(final_c1_valid_RandIndex, final_c2_valid_RandIndex)$AR ==
                 0)
   expect_true(abs(
@@ -49,7 +49,7 @@ test_that("Check ev_rand with wrong inputs", {
                        data.frame(name = letters[1:4], vals = 1:4)))
   expect_error(ev_rand(data.frame(name = letters[1:4], vals = 1:4),
                        c(6, 8, 3, 8)))
-  expect_error(ev_rand(E[1:10, 1], E[, 2]))
+  expect_error(ev_rand(E_LCE[1:10, 1], E_LCE[, 2]))
   expect_error(ev_rand(matrix(c(1, 2, 3), ncol = 3),
                        matrix(c(1, 2, 3), ncol = 3)))
 })
