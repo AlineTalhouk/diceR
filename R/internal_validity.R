5f8aa8a9b66bf3d8650de8092887b54ac7a12a6b
gliu@bccrc.ca <g.liu@alumni.ubc.ca>
2016-10-06 17:06:18 -0700

    added pracma:: and changed parameter x to data because with x it didn't work. Now it works

--- R/internal_validity.R
+++ R/internal_validity.R
@@ -7,7 +7,7 @@
 #' \code{iv_compactness} computes the compactness validity index for a
 #' clustering result
 #'
-#' @param x a dataset with rows as observations, columns sariables
+#' @param data a dataset with rows as observations, columns sariables
 #' @param labels a vector of cluster labels from a clustering result
 #'
 #' @return \code{iv_compactness} returns the compactness score
@@ -15,12 +15,12 @@
 #' @export
 #'
 #' @examples
-#' data("FGD", "E", "FGT", "FGD")
-#' iv_compactness(FGD, E[, 1])
+#' data("FGD", "E_LCE", "FGT", "FGD")
+#' iv_compactness(FGD, E_LCE[, 1])
 #' iv_db_dunn(FGD, FGT)
 #' iv_sumsq(data = FGD, labels = FGT, k = 4)
-iv_compactness <- function(x, labels) {
-  assertthat::assert_that(is.data.frame(x), length(labels) == nrow(x))
+iv_compactness <- function(data, labels) {
+  assertthat::assert_that(is.data.frame(data), length(labels) == nrow(data))
   n <- length(labels)
   C <- sort(unique(labels))
   k <- length(C)
@@ -32,7 +32,7 @@ iv_compactness <- function(x, labels) {
       cp <- cp + 0
     } else{
       sum_d <- 0
-      sum_d <- sum(dist(x[ind, ], method = "euclidean"))
+      sum_d <- sum(dist(data[ind, ], method = "euclidean"))
       cp <- cp + (nk * (sum_d / (nk * (nk - 1) / 2)))
     }
   }
@@ -45,28 +45,28 @@ iv_compactness <- function(x, labels) {
 #'   \item{Dunn}{Dunn score}
 #' @rdname internal_validity
 #' @export
-iv_db_dunn <- function(x, labels) {
+iv_db_dunn <- function(data, labels) {
   if (is.data.frame(labels)) {
-    assertthat::assert_that(nrow(x) == nrow(labels))
+    assertthat::assert_that(nrow(data) == nrow(labels))
   } else if (is.vector(labels)) {
-    assertthat::assert_that(nrow(x) == length(labels))
+    assertthat::assert_that(nrow(data) == length(labels))
   } else{
     stop(
       "labels is neither a vector nor a data frame with length equal to number of observations in the data set."
     )
   }
-  assertthat::assert_that(is.data.frame(x))
-  nrow <- nrow(x)
-  nc <- ncol(x)
+  assertthat::assert_that(is.data.frame(data))
+  nrow <- nrow(data)
+  nc <- ncol(data)
   k <- max(labels)
-  temp <- iv_sumsq(x, labels, k)
+  temp <- iv_sumsq(data, labels, k)
   st <- temp$Tot
   sw <- temp$W
   sb <- temp$B
   cintra <- temp$Sintra
   cinter <- temp$Sinter
-  R <- zeros(k)
-  dbs <- zeros(1, k)
+  R <- pracma::zeros(k)
+  dbs <- pracma::zeros(1, k)
   for (i in 1:k) {
     for (j in (i + 1):k) {
       if (j <= k) {
@@ -101,21 +101,21 @@ iv_db_dunn <- function(x, labels) {
 #'   \item{Sinter}{linkage distance}          
 #' @rdname internal_validity
 #' @export
-iv_sumsq <- function(x, labels, k) {
+iv_sumsq <- function(data, labels, k) {
   assertthat::assert_that(is.vector(labels) || is.data.frame(labels))
   if (is.data.frame(labels)) {
-    assertthat::assert_that(nrow(x) == nrow(labels))
+    assertthat::assert_that(nrow(data) == nrow(labels))
   } else{
-    assertthat::assert_that(nrow(x) == length(labels))
+    assertthat::assert_that(nrow(data) == length(labels))
   }
   assertthat::assert_that(is_pos_int(k))
-  if (is.data.frame(x)) {
-    data <- as.matrix(x)
+  if (is.data.frame(data)) {
+    data <- as.matrix(data)
   }
-  ncase <- nrow(x)
-  m <- ncol(x)
+  ncase <- nrow(data)
+  m <- ncol(data)
   Dm <- t(colMeans(data))
-  Dm <- x - Dm[ones(ncase, 1), ]
+  Dm <- data - Dm[pracma::ones(ncase, 1), ]
   Tot <- t(Dm) %*% Dm
   W <- matrix(rep(0, nrow(Tot) * ncol(Tot)), nrow = nrow(Tot))
   Dm <- matrix(rep(0, k * m), nrow = k)
@@ -128,10 +128,10 @@ iv_sumsq <- function(x, labels, k) {
     }
     nk <- length(Cindex)
     if (nk > 1) {
-      dataC <- x[Cindex, ]
+      dataC <- data[Cindex, ]
       m <- colMeans(dataC)
       Dm[i, ] <- m
-      dataC <- dataC - repmat(m, nk, 1)
+      dataC <- dataC - pracma::repmat(m, nk, 1)
       W <- W + t(dataC) %*% dataC
       dataC <- rowSums(dataC ^ 2)
       Sintra[i] <- mean(sqrt(dataC))
