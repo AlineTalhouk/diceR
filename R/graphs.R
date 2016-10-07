726c84839b2a3363075413a6263b2f7e6af77d18
Derek Chiu <dchiu@bccrc.ca>
2016-10-07 11:54:57 -0700

    consensus matrix heatmaps: docs and unit tests

--- R/graphs.R
+++ R/graphs.R
@@ -2,11 +2,9 @@
 #' 
 #' Graph CDF for consensus matrices from different algorithms
 #' 
-#' @param x an object from \code{consensus_combine} using option \code{element
-#'   = "matrix"}. A list of consensus matrices from different clustering
-#'   algorithms is the input.
+#' @param x an object from \code{\link{ConClust}}
 #'   
-#' @return a ggplot showing the cdfs for different algortihms
+#' @return a plot showing the cdfs for different algortihms
 #' @export
 #' @author Derek Chiu
 #'   
@@ -16,24 +14,58 @@
 #' x <- matrix(rnorm(1000), nrow = 10)
 #' CC1 <- ConClust(x, k = 4, reps = 10,
 #' method = c("hcAEucl", "apEucl", "gmmBIC"), save = FALSE)
-#' CC1.summ <- consensus_summary(CC1, k = 4)
-#' y <- consensus_combine(CC1.summ, element = "matrix")
 #' 
 #' # Plot CDF
-#' p <- graph_cdf(y)
+#' p <- graph_cdf(CC1)
 #' p
 #' 
 #' # Change y label and add colours
 #' p + labs(y = "Probability") + stat_ecdf(aes(colour = Method)) +
 #' scale_color_brewer(palette = "Set2")
+#' 
+#' # Heatmaps with column side colours corresponding to clusters for each algorithm
+#' graph_heatmap(CC1)
 graph_cdf <- function(x) {
-  dat <- lapply(x, function(d) d[lower.tri(d, diag = TRUE)]) %>% 
+  dat <- consensus_summary(x, k = n_distinct(x[, 1, 1], na.rm = TRUE)) %>% 
+    consensus_combine(element = "matrix") %>% 
+    lapply(function(d) d[lower.tri(d, diag = TRUE)]) %>% 
     as.data.frame() %>% 
     tidyr::gather(key = Method, value = CDF, dplyr::everything())
   p <- ggplot(dat, aes(x = CDF)) +
     stat_ecdf() +
     facet_wrap(~Method) +
-    labs(y = "Prob",
-         title = "Cumulative Distribution Function for each Consensus Matrix")
+    labs(x = "Consensus Index",
+         y = "CDF",
+         title = "Consensus Cumulative Distribution Function")
   return(p)
+}
+
+#' Consensus matrix heatmaps
+#' 
+#' @param x an object from \code{\link{ConClust}}
+#' @param main heatmap title
+#' @param ... additional arguments to \code{\link[gplots]{heatmap.2}}
+#' 
+#' @return consensus matrix heatmaps for each algorithm in \code{x}
+#' @author Derek Chiu
+#' @export
+graph_heatmap <- function(x, main = NULL, ...) {
+  k <- n_distinct(x[, 1, 1], na.rm = TRUE)
+  dat <- consensus_summary(x, k = k) %>% 
+    consensus_combine(element = "matrix") 
+  if (is.null(main)) {
+    main <- names(dat)
+  } else {
+    assertthat::assert_that(length(main) == dim(x)[3])
+  }
+  hm.col <- colorRampPalette(brewer.pal(n = 9, "PuBuGn"))(256)
+  cc <- lapply(dat, function(d) brewer.pal(k, "Set2")[cutree(as.dendrogram(
+    hclust(dist(d), method = "average")), k = k)])
+  hm <- mapply(function(dat, main, cc)
+    gplots::heatmap.2(x = dat, main = paste(main, "Consensus Matrix"),
+                      hclustfun = function(x) hclust(x, method = "average"),
+                      trace = "none", dendrogram = "column", col = hm.col,
+                      labRow = "", labCol = "", ColSideColors = cc, ...),
+    dat = dat, main = main, cc = cc)
+  return(hm)
 }
--- tests/testthat/test-graphs.R
+++ tests/testthat/test-graphs.R
@@ -5,9 +5,7 @@ set.seed(911)
 x <- matrix(rnorm(1000), nrow = 10)
 CC1 <- ConClust(x, k = 4, reps = 10, method = c("hcAEucl", "apEucl", "gmmBIC"),
                 save = FALSE)
-CC1.summ <- consensus_summary(CC1, k = 4)
-y <- consensus_combine(CC1.summ, element = "matrix")
-p1 <- graph_cdf(y)
+p1 <- graph_cdf(CC1)
 
 test_that("graph_cdf object can have added/modified ggplot layers", {
   p2 <- p1 +
@@ -18,4 +16,14 @@ test_that("graph_cdf object can have added/modified ggplot layers", {
   expect_is(p1, "ggplot")
   expect_is(p2, "ggplot")
   expect_false(isTRUE(all.equal(p1, p2)))
+})
+
+test_that("graph_heatmap can have same plot but different titles", {
+  phm1 <- graph_heatmap(CC1)
+  phm2 <- graph_heatmap(CC1, main = c("A", "B", "C"))
+  expect_identical(phm1, phm2)
+})
+
+test_that("error in graph_heatmap if too few titles", {
+  expect_error(graph_heatmap(CC1, main = "A"))
 })
