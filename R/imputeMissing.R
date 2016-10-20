766bc2463dc73d229b87e8a31e4c63deffa4c2a2
Aline Talhouk <atalhouk@bccrc.ca>
2016-10-19 23:23:44 -0700

    Function to impute missing values

---
+++ R/imputeMissing.R
@@ -0,0 +1,25 @@
+#' Impute missing values due to subsampling
+#'
+#' Uses k nearest neighbour to impute missing values due to sampling, not all cases will be imputed using this method. If imputeALL is set to true (default), then cases will be relabelled and missing cases will be called by majority vote
+#' 
+#' #' @param E a matrix of clusterings with number of rows equal to the number of cases to be clustered, number of columns equal to the clustering obtained by different resampling of the data, and the third dimension is the different algorithms.
+#' @param data is data matrix with samples as rows and genes/features as columns
+#' @return a flat matrix of clusterings, fully imputed and relabelled.
+#' @author Aline Talhouk
+#' @export
+#' @examples
+
+# E <- ConClust(hgsc, k = 4, reps = 10, method = c("hcAEucl","kmEucl","scRbf"))
+# dat <- t(hgsc[,-1])
+# imputeMissing(E,dat)
+
+imputeMissing <- function(E, data, imputeALL=TRUE)
+# Flatten the matrix
+dim(E_imputed) <- c(dim(E)[1], dim(E)[2]*dim(E)[3])
+# knn impute
+E_imputed <- apply(E_flat,2, knn_impute, data= data)
+# Relabel and Majority vote
+if (imputeALL){
+E_imputed2 <- cbind(E_imputed[,1],
+             apply(E_imputed[,-1],2,function(x){relabel_class(x,E_imputed[,1])})
+}
