53f6d6b62d1db1db63aec85988e94697302fd6b3
gliu@bccrc.ca <g.liu@alumni.ubc.ca>
2016-10-20 11:45:11 -0700

    added test for imputeMissing

--- R/imputeMissing.R
+++ R/imputeMissing.R
@@ -1,24 +1,24 @@
 #' Impute missing values due to subsampling
-#' 
-#' Uses k nearest neighbours to impute missing values due to sampling. Not all
-#' cases will be imputed using this method.
-#' 
-#' @param E a matrix of clusterings with number of rows equal to the number of 
-#'   cases to be clustered, number of columns equal to the clustering obtained 
-#'   by different resampling of the data, and the third dimension is the 
-#'   different algorithms.
+#'
+#' Uses k nearest neighbour to impute missing values due to sampling, not all cases will be imputed using this method. If imputeALL is set to true (default), then cases will be relabelled and missing cases will be called by majority vote
+#'
+#' @param E a matrix of clusterings with number of rows equal to the number of cases to be clustered, number of columns equal to the clustering obtained by different resampling of the data, and the third dimension is the different algorithms.
 #' @param data is data matrix with samples as rows and genes/features as columns
-#' @param imputeALL logical; if \code{TRUE} (default), then cases will be 
-#'   relabelled and missing cases will be called by majority vote
+#' @param imputeALL if FALSE the function will only call knn_impute to impute some NAs, if TRUE, the function will use knn_impute and majority voting to eliminate NAs and relabel class
 #' @return a flat matrix of clusterings, fully imputed and relabelled.
 #' @author Aline Talhouk
 #' @export
 #' @examples
 #' data(hgsc)
-#' dat <- t(hgsc[, -1])
-#' E <- ConClust(dat, k = 4, reps = 10, method = c("hcAEucl", "kmEucl", "scRbf"))
-#' imputeMissing(E, dat)
+#' data <- t(hgsc[,-1])
+#' E <- ConClust(data, k = 4, reps = 10, method = c("hcAEucl","kmEucl","scRbf"))
+#' imputeMissing(E,data)
+
 imputeMissing <- function(E, data, imputeALL = TRUE) {
+  assertthat::assert_that(is.array(E))
+  assertthat::assert_that(is.matrix(data))
+  assertthat::assert_that(dim(E)[1]==nrow(data))
+  assertthat::assert_that(imputeALL==TRUE || imputeALL==FALSE)
   # Flatten the matrix
   E_flat <- E
   dim(E_flat) <- c(dim(E)[1], dim(E)[2] * dim(E)[3])
@@ -35,7 +35,7 @@ imputeMissing <- function(E, data, imputeALL = TRUE) {
       return(x)
     }))
     return(apply(E_imputed2, 2, as.numeric))
-  } else {
+  } else{
     return(E_imputed)
   }
 }
---
+++ tests/testthat/test-imputeMissing.R
@@ -0,0 +1,31 @@
+context("imputeMissing")
+
+test_that("Check imputeMissing with hgsc data with imputeALL TRUE", {
+  data(hgsc)
+  data <- t(hgsc[,-1])
+  E <- ConClust(data, k = 4, reps = 10, method = c("hcAEucl","kmEucl","scRbf"))
+  E_imputed<-imputeMissing(E,data)
+  expect_true(sum(!complete.cases(E_imputed))==0)
+  expect_equal(nrow(E_imputed),dim(E)[1])
+  expect_equal(ncol(E_imputed),dim(E)[2]*dim(E)[3])
+})
+
+test_that("Check imputeMissing with hgsc data with imputeALL FALSE",{
+  data(hgsc)
+  data<-t(hgsc[,-1])
+  E <- ConClust(data, k = 4, reps = 10, method = c("hcAEucl","kmEucl","scRbf"))
+  E_imputed<-imputeMissing(E,data,imputeALL=FALSE)
+  expect_true(sum(!complete.cases(E_imputed))<sum(!complete.cases(E)))
+  expect_equal(nrow(E_imputed),dim(E)[1])
+  expect_equal(ncol(E_imputed),dim(E)[2]*dim(E)[3])
+})
+
+test_that("Check imputeMissing throws error with wrong inputs",{
+  data(hgsc)
+  data<-t(hgsc[,-1])
+  E <- ConClust(data, k = 4, reps = 10, method = c("hcAEucl","kmEucl","scRbf"))
+  expect_error(imputeMissing(E,data[-1,]))
+  expect_error(imputeMissing(E=sample(1:489),data=data))
+  expect_error(imputeMissing(E=E,data=as.data.frame(data)))
+  expect_error(imputeMissing(E=E,data=data,imputeALL = 2))
+})
