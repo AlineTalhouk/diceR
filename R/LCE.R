1a224e8dc9e8007b1e82f9f1ed31dd153b061401
gliu@bccrc.ca <g.liu@alumni.ubc.ca>
2016-10-24 14:55:43 -0700

    changed LCE so default just CTS no more SRSRS and ASRS

--- R/LCE.R
+++ R/LCE.R
@@ -22,13 +22,19 @@
 #'               method = c("nmfEucl", "hcAEucl", "hcDianaEucl"), save = FALSE)
 #' y <- link_clust(E = x, dcCTS = 0.8, dcSRS = 0.8, dcASRS = 0.8, R = 10)
 LCE <- function(E, dcCTS = 0.8, dcSRS = 0.8, dcASRS = 0.8, R = 10,
+                similarity_matrices="cts",
                 is.relabelled = TRUE) {
   assertthat::assert_that(is.array(E) || is.matrix(E), dcCTS >= 0 && dcCTS <= 1,
                           dcASRS >= 0 && dcASRS <= 1, dcSRS >= 0 && dcSRS <= 1,
-                          is_pos_int(R))
-  list(
-    cts = cts(E = E, dc = dcCTS),
-    srs = srs(E = E, dc = dcSRS, R = R),
-    asrs = asrs(E = E, dc = dcASRS)
-  )
+                          is_pos_int(R), sum(!similarity_matrices%in%c("cts","asrs","srs"))==0)
+  CTS=cts(E=E,dc=dcCTS)
+  ASRS<-NULL
+  SRS<-NULL
+  if("asrs"%in%similarity_matrices){
+    ASRS<-asrs(E=E,dc=dcASRS)
+  }
+  if("srs"%in%similarity_matrices){
+    SRS<-srs(E=E,dc=dcSRS,R=R)
+  }
+  return(list(CTS=CTS,SRS=SRS,ASRS=ASRS))
 }
