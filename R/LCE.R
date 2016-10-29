8fd42e742f99087def1835fc44ce124766210537
Derek Chiu <dchiu@bccrc.ca>
2016-10-28 17:25:52 -0700

    mod LCE after example/tests after return value changed

--- R/LCE.R
+++ R/LCE.R
@@ -5,30 +5,30 @@
 #' @param E is an array of clustering results. Missing entries are imputed using
 #'   \code{imputeMissing}
 #' @param data original data matrix with rows as samples, columns as variables
+#' @param k requested number of clusters
 #' @param dcCTS decay constant for CTS matrix
 #' @param dcSRS decay constant for SRS matrix
 #' @param dcASRS decay constant for ASRS matrix
 #' @param R number of repetitions for SRS matrix
 #' @param sim.mat vector of similarity matrices to compute. Choices are "cts",
 #'   "srs", "asrs".
-#' @param is.relabelled logical; defaults to \code{TRUE}, but if \code{FALSE}
-#'   the data will be relabelled
 #' @author Johnson Liu
-#' @return a list containing the CTS, SRS, and ASRS matrices (as specified)
+#' @return a vector containing the cluster assignment from either the CTS, SRS,
+#'   or ASRS similarity matrices
 #' @export
 #' @examples
 #' data(hgsc)
 #' dat <- t(hgsc[, -1])[1:100, 1:50]
 #' x <- ConClust(dat, nc = 4, reps = 4,
 #'               method = c("nmfEucl", "hcAEucl", "hcDianaEucl"), save = FALSE)
-#' y <- LCE(E = x, data = dat, dcCTS = 0.8, dcSRS = 0.8, dcASRS = 0.8, R = 10,
+#' LCE(E = x, data = dat, k = 4, dcCTS = 0.8, dcSRS = 0.8, dcASRS = 0.8, R = 10,
 #' sim.mat = "asrs")
-LCE <- function(E, data, dcCTS = 0.8, dcSRS = 0.8, dcASRS = 0.8, R = 10,
-                sim.mat = c("cts", "srs", "asrs"), is.relabelled = TRUE) {
+LCE <- function(E, data, k, dcCTS = 0.8, dcSRS = 0.8, dcASRS = 0.8, R = 10,
+                sim.mat = c("cts", "srs", "asrs")) {
   assertthat::assert_that(is.array(E), dcCTS >= 0 && dcCTS <= 1,
                           dcASRS >= 0 && dcASRS <= 1, dcSRS >= 0 && dcSRS <= 1,
                           is_pos_int(R))
-  E2 <- imputeMissing(E, data, imputeALL = TRUE)[, , 1]
+  E2 <- imputeMissing(E, data, imputeALL = TRUE)$E_imputed2[, , 1]
   S <- switch(match.arg(sim.mat),
               cts = cts(E = E2, dc = dcCTS),
               srs = srs(E = E2, dc = dcSRS, R = R),
