07bfc0b7ce7975e5cce21f6e709df266a8d085b6
Derek Chiu <dchiu@bccrc.ca>
2016-11-04 11:22:18 -0700

    add back E_imputed: needed for k_modes, majority_voting examples

--- R/data.R
+++ R/data.R
@@ -1,3 +1,10 @@
+#' Example data for imputation
+#'
+#' The data set is intended to be tested with \code{\link{knn_impute}}
+#'
+#' @format An array with 489 rows, 10 columns, 12 slices
+"E_imputed"
+
 #' High grade serous cancer data from TCGA used to classify subtypes
 #'
 #' There are 321 genes and 489 samples. The first column is a gene identifier.
