aef003bd4f176aa16b08dbbda4143f7e5532b7c1
gliu@bccrc.ca <g.liu@alumni.ubc.ca>
2016-11-04 12:50:25 -0700

    added documentation for conClustTest_out.rds

--- R/data.R
+++ R/data.R
@@ -11,3 +11,10 @@
 #'
 #' @format A data frame with 321 rows and 490 variables
 "hgsc"
+
+#' Output of diceR::ConClust using HGSC TCGA data with all 12 default algorithms and 50 repetitions
+#'
+#' The 1st dimension indicates number of samples (489), second dimension is number of repetitions (50), third dimension is number of algorithms
+#'
+#' @format An arrary of dimension 489 by 50 by 12 by 1
+"hgsc"
