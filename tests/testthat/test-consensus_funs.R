d3b4d02734df5af0a2be41001fd1da7c0ae668d1
Derek Chiu <dchiu@bccrc.ca>
2016-11-14 15:53:14 -0800

    update consensus_funs unit tests after CSPA

--- tests/testthat/test-consensus_funs.R
+++ tests/testthat/test-consensus_funs.R
@@ -1,6 +1,13 @@
 
 context("Consensus functions")
 
+data(hgsc)
+dat <- t(hgsc[, -1])[1:200, 1:100]
+k <- 4
+x <- ConClust(dat, nk = k, reps = 4, progress = FALSE,
+              method = c("nmfEucl", "hcAEucl", "hcDianaEucl"))
+x_imputed <- impute_missing(x, dat)$complete
+
 test_that("majority voting works", {
   dt <- array(c(2, 3, 2, 2, 2, 3, 1, 1, 2, 3, 2, 2, 2, 3, 1, 1), c(2, 4, 2))
   expect_equal(majority_voting(dt, is.relabelled = TRUE), c(2, 3))
@@ -20,30 +27,19 @@ test_that("k modes works with or without missing", {
   expect_false(anyNA(kmo.missing))
 })
 
-set.seed(1)
-x <- replicate(100, rbinom(100, 4, 0.2))
-y <- consensus_matrix(x)
-
 test_that("CSPA works", {
-  expect_length(CSPA(y, k = 2), nrow(y))
-  expect_equal(dplyr::n_distinct(CSPA(y, k = 2)), 2)
-  expect_equal(dplyr::n_distinct(CSPA(y, k = 3)), 3)
+  expect_length(CSPA(x, k = 4), nrow(x))
+  expect_equal(dplyr::n_distinct(CSPA(x, k = 4)), 4)
 })
 
 test_that("Check LCE with hgsc data with 3 ConClust algorithms", {
-  data(hgsc)
-  dat <- t(hgsc[, -1])[1:200, 1:100]
-  k <- 4
-  x <- ConClust(dat, nk = k, reps = 4, progress = FALSE,
-                method = c("nmfEucl", "hcAEucl", "hcDianaEucl"))
-  x_imputed <- impute_missing(x, dat)$complete
-  y_cts <- LCE(E = x_imputed, data = dat, k = k, R = 5, sim.mat = "cts")
-  y_srs <- LCE(E = x_imputed, data = dat, k = k, R = 5, sim.mat = "srs")
-  y_asrs <- LCE(E = x_imputed, data = dat, k = k, R = 5, sim.mat = "asrs")
+  y_cts <- LCE(E = x_imputed, k = k, R = 5, sim.mat = "cts")
+  y_srs <- LCE(E = x_imputed, k = k, R = 5, sim.mat = "srs")
+  y_asrs <- LCE(E = x_imputed, k = k, R = 5, sim.mat = "asrs")
   expect_length(y_cts, 200)
   expect_length(y_srs, 200)
   expect_length(y_asrs, 200)
-  expect_is(y_cts, "factor")
-  expect_is(y_srs, "factor")
-  expect_is(y_asrs, "factor")
+  expect_is(y_cts, "integer")
+  expect_is(y_srs, "integer")
+  expect_is(y_asrs, "integer")
 })
