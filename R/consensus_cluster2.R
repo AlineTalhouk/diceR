e831a9af9855d3a335c732684c09bf978bd25187
Derek Chiu <dchiu@bccrc.ca>
2016-11-23 13:01:38 -0800

    custom distance works

--- R/consensus_cluster2.R
+++ R/consensus_cluster2.R
@@ -3,41 +3,37 @@
 #' @examples 
 #' data(hgsc)
 #' dat <- t(hgsc[, -1])
-#' test <- consensus_cluster(dat, reps = 5)
-#' test2 <- consensus_cluster2(dat, reps = 5)
+#' test <- consensus_cluster(dat, reps = 5, algorithms = c("nmfDiv", "hcAEucl", "scRbf"))
+#' test2 <- consensus_cluster2(dat, reps = 5, algorithms = c("nmf", "hc", "sc"), nmf.method = "brunet", distance = c("euclidean", "myDistFunc"))
 #' str(test)
 #' str(test2)
+#' identical(unname(test), unname(test2))
 consensus_cluster2 <- function(data, nk = 2:4, pItem = 0.8, reps = 1000,
                                algorithms = NULL, nmf.method = c("brunet", "lee"),
                                distance = "euclidean",
-                               parallel = NULL, ncores = NULL,
                                progress = TRUE, seed = 123456, seed.alg = 1,
                                min.sd = 1, save = FALSE, file.name = "CCOutput",
                                time.saved = FALSE) {
-  x.rest <- prepare_data(data, min.sd = min.sd)
+  data.prep <- prepare_data(data, min.sd = min.sd)
+  nmf.arr <- other.arr <- dist.arr <- NULL
   if (is.null(algorithms))
     algorithms <- c("nmf", "hc", "diana", "km", "pam",
                     "ap", "sc", "gmm", "block")
+  
   if ("nmf" %in% algorithms) {
-    nmf.arr <- cluster_nmf(x.rest, nk, pItem, reps, nmf.method, seed, seed.alg)
-  } else {
-    nmf.arr <- NULL
+    nmf.arr <- cluster_nmf(data.prep, nk, pItem, reps, nmf.method, seed, seed.alg)
   }
   
-  oalgs <- c("ap", "sc", "gmm", "block")
-  if (any(oalgs %in% algorithms)) {
-    other.arr <- cluster_other(x.rest, nk, pItem, reps, oalgs, seed, seed.alg)
-  } else {
-    other.arr <- NULL
+  oalgs <- algorithms[algorithms %in% c("ap", "sc", "gmm", "block")]
+  if (length(oalgs) > 0) {
+    other.arr <- cluster_other(data.prep, nk, pItem, reps, oalgs, seed, seed.alg)
   }
   
-  dalgs <- c("hc", "diana", "km", "pam")
-  if (any(dalgs %in% algorithms)) {
-    dist.arr <- cluster_dist(x.rest, nk, pItem, reps, dalgs, distance, seed, seed.alg)
-  } else {
-    dist.arr <- NULL
+  dalgs <- algorithms[algorithms %in% c("hc", "diana", "km", "pam")]
+  if (length(dalgs) > 0) {
+    dist.arr <- cluster_dist(data.prep, nk, pItem, reps, dalgs, distance, seed, seed.alg)
   }
-
+  
   all.arr <- abind::abind(nmf.arr, dist.arr, other.arr, along = 3)
   return(all.arr)
 }
@@ -88,13 +84,13 @@ cluster_dist <- function(data, nk, pItem, reps, dalgs, distance, seed, seed.alg)
                                           1, function(x) paste0(x[2], "_", x[1])),
                                     nk))
   for (k in 1:lnk) {
-    set.seed(seed.alg)
-    for (i in 1:reps) {
-      ind.new <- sample(n, n.new, replace = FALSE)
-      dists <- distances(data[ind.new, ], distance)
-      for (j in 1:length(dalgs)) {
-        for (d in 1:ld) {
-          dist.arr[ind.new, i, (j - 1) * ld + d, k] <- get(dalgs[j])(dists[[d]], 4)
+    for (j in 1:length(dalgs)) {
+      for (d in 1:ld) {
+        set.seed(seed.alg)
+        for (i in 1:reps) {
+          ind.new <- sample(n, n.new, replace = FALSE)
+          dists <- distances(data[ind.new, ], distance[d])
+          dist.arr[ind.new, i, (j - 1) * ld + d, k] <- get(dalgs[j])(dists[[1]], nk[k])
         }
       }
     }
@@ -162,15 +158,3 @@ km <- function(d, k) {
 pam <- function(d, k) {
   return(as.integer(cluster::pam(d, k, cluster.only = TRUE)))
 }
-
-# algorithms <- c("nmf", "hc", "diana", "km", "pam", "ap", "sc", "gmm", "block")
-# nalgs <- "nmf"
-# dalgs <- c("hc", "diana", "km", "pam")
-# oalgs <- c("ap", "sc", "gmm", "block")
-# distance <- c("eucl", "spear", "myDistFunc")
-# linkage <- "average"
-# nmf.method <- c("brunet", "lee")
-# nnmf <- length(nmf.method)
-# methods <- c("NMF_Div", "NMF_Eucl", "HC_Eucl", "HC_Spear",
-#              "DIANA_Eucl", "DIANA_Spear", "KM_Eucl", "KM_Spear",
-#              "PAM_Eucl", "PAM_Spear", "AP", "SC", "GMM", "BLOCK")
