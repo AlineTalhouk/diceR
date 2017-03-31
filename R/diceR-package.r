99e7f2d86e06374a985a4f842f15a3524e710af0
Derek Chiu <dchiu@bccrc.ca>
2017-03-31 10:41:00 -0700

    update spacing, file extension capitalized

--- R/diceR-package.r
+++ R/diceR-package.r
@@ -2,7 +2,7 @@
 #' 
 #' The diceR package performs Consensus Clustering, an ensemble clustering 
 #' algorithm that pools together results from different clustering realizations.
-#' Each realization uses a unique combination of observations and diverse
+#' Each realization uses a unique combination of observations and diverse 
 #' clustering algorithms.
 #' 
 #' @name diceR-package
