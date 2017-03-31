6750e3ddc24e4f2f57a6fd1cbb2cc74651f1f333
Billy Chen <bchen@ctagmac28.bccrc.ca>
2017-03-31 10:53:30 -0700

    LCE files

---
+++ TCGA/R_file/prep/prep.R
@@ -0,0 +1,5 @@
+pr<- "cs"
+sfdir<- "/Users/bchen/Desktop/HGSCdata/outputs/TCGA"
+ndat<- "TCGA"
+datadir<- "/Users/bchen/Desktop/HGSCdata/outputs/TCGA/data_pr"
+source("/Users/bchen/Desktop/diceR/0_read_data.R")
---
+++ TCGA/sh_file/prep/prep.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+export PATH=/share/data/apps/R/R-3.2.5/bin:$PATH
+Rscript /Users/bchen/Desktop/diceR/TCGA/R_file/prep/prep.R
