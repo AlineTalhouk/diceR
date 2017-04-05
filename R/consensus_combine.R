232e580ed6b285bc86488f87e64b8910115abe49
Derek Chiu <dchiu@bccrc.ca>
2017-04-05 13:52:15 -0700

    remove export of consensus_trim, separate docs

--- R/consensus_combine.R
+++ R/consensus_combine.R
@@ -1,32 +1,19 @@
-#' Combine, evaluate, trim, and reweigh algorithms
-#'
-#' \code{consensus_combine} combines results for multiple objects from
-#' \code{consensus_cluster()} and outputs either the consensus
-#' matrices or consensus classes for all algorithms. \code{consensus_evaluate}
-#' evaluates algorithms on internal/external validation indices.
-#' \code{consensus_weigh} weighs clustering algorithms based on these two
-#' indices. \code{consensus_trim} removes algorithms that rank low on internal
-#' indices before using in consensus functions.
-#'
-#' \code{consensus_combine} is useful for generating summaries because the
-#' results have been combined into a single object. For example, if
-#' \code{element = "class"}, then the resulting object can be used to create a
-#' consensus matrix across algorithms, which can be visualized as a heatmap.
+#' Combine algorithms
 #' 
-#' \code{consensus_evaluate} always shows internal indices. If \code{ref.cl} is
-#' not \code{NULL}, external indices are shown in addition to internal indices.
-#' Relevant graphical displays are also outputted.
+#' Combines results for multiple objects from \code{consensus_cluster()} and
+#' outputs either the consensus matrices or consensus classes for all
+#' algorithms.
 #' 
-#' \code{consensus_trim} ranks algorithms by internal indices used in
-#' \code{consensus_evaluate}. The sum of the ranks for each algorithm is used as
-#' the measure of comparison. This also means the magnitude of the internal
-#' indices is not taken into account.
-#'
-#' @param ... any number of objects outputted from
+#' This function is useful for collecting summaries because the original results
+#' from \code{consensus_cluster} were combined to a single object. For example,
+#' setting \code{element = "class"} returns a matrix of consensus cluster
+#' assignments, which can be visualized as a consensus matrix heatmap.
+#' 
+#' @param ... any number of objects outputted from 
 #'   \code{\link{consensus_cluster}}
-#' @param element either "matrix" or "class" to extract the consensus matrix or
+#' @param element either "matrix" or "class" to extract the consensus matrix or 
 #'   consensus class, respectively.
-#' @return \code{consensus_combine} returns either a list of all consensus
+#' @return \code{consensus_combine} returns either a list of all consensus 
 #'   matrices or a data frame showing all the consensus classes
 #' @author Derek Chiu
 #' @export
@@ -44,15 +31,6 @@
 #' str(y1)
 #' y2 <- consensus_combine(CC1, CC2, element = "class")
 #' str(y2)
-#' 
-#' # Evaluate algorithms on internal and external indices and make plots
-#' set.seed(1)
-#' ref.cl <- sample(1:4, 50, replace = TRUE)
-#' z <- consensus_evaluate(x, CC1, CC2, ref.cl = ref.cl, plot = FALSE)
-#' 
-#' # Trim algorithms: remove those that rank low on internal indices
-#' CC3 <- consensus_trim(x, CC1, CC2, ref.cl = ref.cl, n = 1)
-#' str(CC3, max.level = 2)
 consensus_combine <- function(..., element = c("matrix", "class")) {
   # Combine ensemble arrays and reorganize into matrices and classes
   cs <- abind::abind(list(...), along = 3)
--- R/consensus_evaluate.R
+++ R/consensus_evaluate.R
@@ -1,3 +1,15 @@
+#' Evaluate, trim, and reweigh algorithms
+#' 
+#' Evaluates algorithms on internal/external validation indices. Poor performing
+#' algorithms can be trimmed from the ensemble. The remaining algorithms can be
+#' given weights before use in consensus functions.
+#' 
+#' This function always returns internal indices. If \code{ref.cl} is not
+#' \code{NULL}, external indices are additionally shown. Relevant graphical
+#' displays are also outputted. Algorithms are ranked across internal indices
+#' using Rank Aggregation. Only the top \code{n} algorithms are kept, the rest
+#' are trimmed.
+#' 
 #' @param data data matrix with rows as samples and columns as variables
 #' @param cons.cl matrix of cluster assignments from consensus functions such
 #'   as \code{kmodes} and \code{majority_voting}
@@ -25,8 +37,26 @@
 #'   indices}
 #'   \item{external}{a data frame showing external evaluation indices for
 #'   \code{k}}
-#' @rdname consensus_combine
+#'   \item{trim}{A list with 3 elements}
+#'   \itemize{
+#'     \item{alg.keep}{algorithms kept}
+#'     \item{alg.remove}{algorithms removed}
+#'     \item{data.new}{A new version of a \code{consensus_cluster} data object.}
+#'   }
 #' @export
+#' @examples 
+#' # Consensus clustering for multiple algorithms
+#' set.seed(911)
+#' x <- matrix(rnorm(500), ncol = 10)
+#' CC <- consensus_cluster(x, nk = 3:4, reps = 10, algorithms = c("ap", "gmm"),
+#' progress = FALSE)
+#' 
+#' # Evaluate algorithms on internal/external indices and trim algorithms:
+#' # remove those ranking low on internal indices
+#' set.seed(1)
+#' ref.cl <- sample(1:4, 50, replace = TRUE)
+#' z <- consensus_evaluate(x, CC, ref.cl = ref.cl, n = 1, trim = TRUE)
+#' str(z, max.level = 2)
 consensus_evaluate <- function(data, ..., cons.cl = NULL, ref.cl = NULL,
                                k.method = NULL, plot = FALSE, trim = FALSE,
                                reweigh = FALSE, n = 5) {
@@ -125,6 +155,98 @@ consensus_evaluate <- function(data, ..., cons.cl = NULL, ref.cl = NULL,
   
   return(list(k = k, pac = pac, internal = ind.int, external = ind.ext,
               trim = trim.obj))
+}
+
+#' @param E consensus object from \code{consensus_evaluate}
+#' @param ii internal indices object from \code{consensus_evaluate}
+#' @param k chosen value(s) of k from \code{consensus_evaluate}
+#' @param n an integer specifying the top \code{n} algorithms to keep after 
+#'   trimming off the poor performing ones using Rank Aggregation. If the total
+#'   number of algorithms is less than \code{n} no trimming is done.
+#' @param reweigh logical; if \code{TRUE}, after trimming out poor performing
+#'   algorithms, each algorithm is reweighed depending on its internal indices.
+#' @noRd
+consensus_trim <- function(E, ii, k, n = 5, reweigh = FALSE) {
+  k <- as.character(min(k))
+  zk <- ii[[k]]
+  alg.all <- dimnames(E)[[3]]
+  
+  # Separate algorithms into those from clusterCrit (main), and (others)
+  z.main <- zk %>% 
+    extract(!names(.) %in% c("Algorithms", "Compactness", "Connectivity") &
+              purrr::map_lgl(., ~ all(!is.nan(.x))))
+  z.other <- zk %>% 
+    extract(c("Compactness", "Connectivity"))
+  
+  # Which algorithm is the best for each index?
+  bests <- purrr::map2_int(z.main, names(z.main), clusterCrit::bestCriterion)
+  
+  # Which indices are the best with the greatest/least value?
+  max.bests <- z.main %>% 
+    extract(purrr::map_int(., which.max) == bests) %>% 
+    cbind(z.other)
+  min.bests <- z.main %>% 
+    extract(purrr::map_int(., which.min) == bests)
+  
+  # Determine trimmed ensemble using rank aggregation, only if there are more
+  # algorithms than we want to keep
+  if (length(alg.all) <= n) {
+    alg.keep <- alg.all
+  } else {
+    max.ranks <- purrr::map_df(max.bests, ~rank(-.x))
+    min.ranks <- purrr::map_df(min.bests, rank)
+    rank.agg <- cbind(max.ranks, min.ranks) %>% 
+      t() %>% 
+      apply(1, function(x) alg.all[x]) %>% 
+      t() %>% 
+      RankAggreg::RankAggreg(., ncol(.), rho = 0.5, verbose = FALSE)
+    alg.keep <- rank.agg$top.list[seq_len(n)]
+  }
+  alg.remove <- as.character(alg.all[!(alg.all %in% alg.keep)])
+  E.trim <- E[, , alg.keep, k, drop = FALSE]
+  
+  # Reweigh only if specified and more than 1 algorithm is kept
+  if (reweigh && length(alg.keep) > 1) {
+    
+    # Filter after knowing which to keep
+    ak <- match(alg.keep, alg.all)
+    max.bests <- max.bests[ak, ]
+    min.bests <- z.main %>% 
+      extract(ak, purrr::map_int(., which.min) == bests) %>% 
+      purrr::map_df(~ sum(.x) - .x)
+    
+    # Create multiples of each algorithm proportion to weight
+    # Divide multiples by greatest common divisor to minimize number of copies
+    multiples <- cbind(as.matrix(max.bests), as.matrix(min.bests)) %>% 
+      prop.table(2) %>% 
+      rowMeans() %>% 
+      multiply_by(100) %>% 
+      round(0) %>% 
+      divide_by(Reduce("gcd", .)) %>% 
+      set_names(alg.keep)
+    
+    # Generate multiples for each algorithm, adding back dimnames metadata
+    E.trim <- purrr::array_branch(E.trim, c(3, 4)) %>% 
+      purrr::map2(., multiples, ~ rep(list(.x), .y)) %>% 
+      purrr::map(abind::abind, along = 3) %>% 
+      abind::abind(along = 3) %>% 
+      abind::abind(along = 4)
+    dimnames(E.trim) <-
+      list(NULL,
+           dimnames(E.trim)[[2]],
+           purrr::flatten_chr(purrr::map2(names(multiples), multiples, rep)),
+           k)
+  }
+  return(list(alg.keep = alg.keep,
+              alg.remove = alg.remove,
+              data.new = E.trim))
+}
+
+#' Recursively find the greater common divisor of two numbers
+#' @noRd
+gcd <- function(x, y) {
+  r <- x %% y
+  return(ifelse(r, gcd(y, r), y))
 }
 
 #' Compactness Measure
