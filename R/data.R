1be681c54c8a926962da76cd3517b9a36f069d0b
Derek Chiu <dchiu@bccrc.ca>
2017-04-19 16:47:53 -0700

    save internal data to R/sysdata.rda, update docs

--- R/data.R
+++ R/data.R
@@ -1,22 +1,6 @@
-#' Example data for imputation
-#'
-#' The data set is used as an example to test imputation
-#'
-#' @format An array with 489 rows, 10 columns, 12 slices
-"E_imputed"
-
-#' High grade serous cancer data from TCGA used to classify subtypes
+#' Gene expression data for High Grade Serous Carcinoma from TCGA
 #'
 #' There are 321 genes and 489 samples. The first column is a gene identifier.
 #'
 #' @format A data frame with 321 rows and 490 variables
 "hgsc"
-
-#' Output of deprecated function ConClust using HGSC TCGA data with all 12
-#' default algorithms and 50 repetitions
-#' 
-#' The 1st dimension indicates number of samples (489), second dimension is 
-#' number of repetitions (50), third dimension is number of algorithms
-#' 
-#' @format An arrary of dimension 489 by 50 by 12 by 1
-"conClust_tcga"
