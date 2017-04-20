80bb48ede4723fb68fb3ea270a05de9a10cd1cb3
Derek Chiu <dchiu@bccrc.ca>
2017-04-19 17:03:01 -0700

    improve doc for hgsc data

--- R/data.R
+++ R/data.R
@@ -1,6 +1,10 @@
 #' Gene expression data for High Grade Serous Carcinoma from TCGA
-#'
-#' There are 321 genes and 489 samples. The first column is a gene identifier.
-#'
-#' @format A data frame with 321 rows and 490 variables
+#' 
+#' There are 489 samples measured on 321 genes. Sample IDs are in the row names 
+#' and gene names are in the column names. This data set is used for clustering 
+#' HGSC into subtypes with prognostic significance. The cluster assignments 
+#' obtained by TCGA are indicated by the last six characters of each row name in
+#' \code{hgsc}: \code{MES.C1}, \code{IMM.C2}, \code{DIF.C4}, and \code{PRO.C5"}
+#' 
+#' @format A data frame with 489 rows and 321 columns.
 "hgsc"
