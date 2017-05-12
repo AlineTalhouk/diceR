3a7e4215059e68b82ec42e4fc3e33a081056e123
Derek Chiu <dchiu@bccrc.ca>
2017-05-12 11:53:30 -0700

    sample on length not values

--- R/consensus_evaluate.R
+++ R/consensus_evaluate.R
@@ -218,7 +218,7 @@ consensus_trim <- function(E, ii, k, k.method, reweigh, n) {
     rank.agg <- cbind(max.bests, min.bests) %>%
       scale(center = FALSE, scale = TRUE) %>%
       as.data.frame() %>%
-      purrr::map_df(~ alg.all[order(.x, sample(.x))]) %>%
+      purrr::map_df(~ alg.all[order(.x, sample(length(.x)))]) %>%
       t()
     top.list <- rank.agg %>%
       RankAggreg::RankAggreg(., ncol(.), method = "GA", verbose = FALSE) %>%
