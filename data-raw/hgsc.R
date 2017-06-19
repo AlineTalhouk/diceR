1ad0a3f1e4d0cca69ca5167f7737d45b36b40306
Derek Chiu <dchiu@bccrc.ca>
2017-06-19 13:12:17 -0700

    add reference class as attribute to hgsc data

--- data-raw/hgsc.R
+++ data-raw/hgsc.R
@@ -1,8 +1,10 @@
 # Transpose while keeping sample ID and gene names in dimnames
+library(magrittr)
 raw <- get(load(file = "data-raw/hgsc-raw.rda"))
-hgsc <- raw %>% 
-  magrittr::set_rownames(.$UNIQID) %>% 
-  magrittr::extract(-1) %>% 
-  t() %>% 
-  as.data.frame()
-devtools::use_data(hgsc, compress = "xz")
+hgsc <- raw %>%
+  set_rownames(.$UNIQID) %>%
+  extract(-1) %>%
+  t() %>%
+  as.data.frame() %>%
+  structure(class.true = gsub(".*_", "\\1", rownames(.)))
+devtools::use_data(hgsc, overwrite = TRUE, compress = "xz")
