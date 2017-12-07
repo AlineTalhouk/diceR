ef26bfa8b8bddd1f956170e18f5a7a63c53b1247
Derek Chiu <dchiu@bccrc.ca>
2017-12-07 13:46:43 -0800

    add helper to suppress cat() printouts

--- R/utils.R
+++ R/utils.R
@@ -120,3 +120,15 @@ is_pos_int <- function(x) {
     }
   }
 }
+
+#' Redirect any console printouts from print() or cat() to null device
+#' @references
+#'   http://stackoverflow.com/questions/5310393/disabling-the-cat-command
+#' @noRd
+sink_output <- function(expr) {
+  tmp <- tempfile()
+  sink(tmp)
+  on.exit(sink())
+  on.exit(file.remove(tmp), add = TRUE)
+  invisible(force(expr))
+}
