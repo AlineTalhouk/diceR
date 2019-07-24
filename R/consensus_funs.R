81e6c981535164b4e25aa05cf3f02693ad28d6e7
Derek Chiu <dchiu@bccrc.ca>
2019-07-24 09:59:19 -0700

    only attach if not already on search path

--- R/consensus_funs.R
+++ R/consensus_funs.R
@@ -153,7 +153,7 @@ LCE <- function(E, k, dc = 0.8, R = 10, sim.mat = c("cts", "srs", "asrs")) {
 #' FALSE)
 #' table(LCA(cc[, , 1, 1, drop = FALSE], is.relabelled = FALSE))
 LCA <- function(E, is.relabelled = TRUE, seed = 1) {
-  attachNamespace("MASS")
+  if (!"package:MASS" %in% search()) attachNamespace("MASS")
   flat_E <- E %>%
     flatten_E(is.relabelled = is.relabelled) %>%
     as.data.frame()
