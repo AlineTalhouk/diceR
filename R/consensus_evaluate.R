d37782af01d68a7a9f2829ac87d0688c22f68cb2
Derek <dchiu@bccrc.ca>
2020-06-29 15:01:29 -0700

    remove old calls to RNGversion

--- R/consensus_evaluate.R
+++ R/consensus_evaluate.R
@@ -327,11 +327,9 @@ gcd <- function(x, y) {
 #' @export
 #'
 #' @examples
-#' suppressWarnings(RNGversion("3.5.0"))
 #' set.seed(1)
 #' E <- matrix(rep(sample(1:4, 1000, replace = TRUE)), nrow = 100, byrow =
 #'               FALSE)
-#' suppressWarnings(RNGversion("3.5.0"))
 #' set.seed(1)
 #' dat <- as.data.frame(matrix(runif(1000, -10, 10), nrow = 100, byrow = FALSE))
 #' compactness(dat, E[, 1])
